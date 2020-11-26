95c060d7c7724ba9bbd3dc505c31378b7df7e411
mzarco15 <mzarco@vt.edu>
2020-11-26 08:21:22 -0500

    added api key

--- services.py
+++ services.py
@@ -1,79 +1,50 @@
 #python code running on services laptop
 #!flask/bin/python
-from flask import Flask
-from flask import jsonify
-from flask import make_response
-from flask_httpauth import HTTPBasicAuth
 
-auth = HTTPBasicAuth()
+from flask import Flask, flash, render_template, abort
+from flask import jsonify
+from flask import request
+from flask import make_response
+from flask import session
+from requests.auth import HTTPBasicAuth
+#api public key for marvel
+#5fd57f8f0bc35903bab675fbfc99d9f7
+####
+# request with auth=username, password
+####
+url = "https://gateway.marvel.com/v1/public/stories/36864?apikey=5fd57f8f0bc35903bab675fbfc99d9f7"
+r = request.get(url, auth=('username', 'password'))
+
 app = Flask(__name__)
 
-if __name__ == '_main_':     
-    app.run(debug=True)
-  
-"task" : {
-    'id' : 1;
-    'done': False;
-}
+if __name__ == '_main_':
+    #run the services   
+    app.run(debug=True, host='0.0.0', port=5000)
+#one route    
+@app.route('/', methods = ['GET'])
+#the second route
+@app.route('/login', methods= ['POST'])
+####
+# capture the sent username and password 
+# and compare to whatever the login credentials saved
+# into the python script
+####
+def do_admin_login():#pythonspot.com
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('Wrong password!')
+    return home()
 
-@app.route('/', methods = ['GET']) 
-@app.login_required
-@app.errorhandler(404)
-@app.get_password
-def index();
-   return "Hello World"
-
-def get_password(username):
-    if username == 'user'
-        return 'yay'
-    return None
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return dosomething() #the main method for serices
 
 def notFound(error):
     return make_response(jsonify({'error':'Not found'}), 404)
 
-def get_task():
-    #jsonify({'tasks':tasks});
-    return jsonify({'tasks':[make_public_task(task) for task in tasks]})
-def get_tasks():
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task' : task[0]})
-
-def make_public_task(task):
-    new_task = {}
-    for field in task:
-        if field == 'id':
-            new_task['uri']  = url_for('get_task', task_id = task['id'], _external = True)
-        else:
-            new_task[field] = task[field]
-    return new_task
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(404)
-    task = {
-        'title' : request.json['title'], 
-        'description': request.json.get('description', "")
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
-
-def update_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    if not request.json :
-        abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode :
-        abort(400)
-    if 'description' in request.json and type(request.json['title']) is not unicode :
-        abort(400)
-    task[0]['title' = request].json.get('title', task[0]['title'])
-    return jsonify({'task' : task[0]})
-
-def delete_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    tasks.remove(task[0])
-    return jsonify({'result' : True})
+def dosomething():
+    print(r.text)
+    return None
