8e694d4e6baf65e80e067272622449f68916c2d5
JoshTheLion <josh13@vt.edu>
2020-12-12 22:07:59 -0500

    Fixed Updat output for submission

--- scraper.py
+++ scraper.py
@@ -1,19 +1,24 @@
 #!flask/bin/python
 """
 ECE 4564 - Assignment 3 - Our API (Scraper Flask)
+
 Before running, first make sure that the IP address you want to use is set for the parameters:
-    SERVICE_IP = '0.0.0.0'
-    SCRAPER_IP = '0.0.0.0'
+    SERVICE_IP = '127.0.0.1'
+    SCRAPER_IP = '127.0.0.1'
 (default values shown, be sure these assignments are consistent in both this file and scraper.py)
+
 Run the Scraper code with the simple command line format:
     $ python3 scraper.py
+
 Once the flask instance is running, test connectivity from a separate terminal window with the command:
-    $ curl -u user1:pass1 http://0.0.0.0:3000/Test/get_scraper_resource
+    $ curl -u user1:pass1 http://127.0.0.1:3000/Test/get_scraper_resource
+
 Supports 3 HTTP `requests` from services.py: ??? I'm just guessing at the format here...
     r = requests.get('https://api.website.com', auth=('user', 'pass'))
     r = requests.get('http://' + SCRAPER_IP + ':' + SCRAPER_PORT + '/Weather/<city>?user=' + {scrape_user} + '&pass=' + {scrape_pass})
     r = requests.get('http://' + SCRAPER_IP + ':' + SCRAPER_PORT + '/COVID/<state>?user=' + {scrape_user} + '&pass=' + {scrape_pass})
     r = requests.post('http://' + SCRAPER_IP + ':' + SCRAPER_PORT + '/Update?user=' + {scrape_user} + '&pass=' + {scrape_pass})
+
 Based on example code from a 4-part series of articles by Miguel Grinberg, at:
     https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
     https://blog.miguelgrinberg.com/post/restful-authentication-with-flask
@@ -38,51 +43,35 @@ app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 # Setting default values
-SERVICE_IP = '0.0.0.0' #('0.0.0.0' or '127.0.0.1' for local testing, 'xxx.x.x.x' format for full online usage)
+SERVICE_IP = '127.0.0.1' #('0.0.0.0' or '127.0.0.1' for local testing, 'xxx.x.x.x' format for full online usage)
 SERVICE_PORT = 8081
 
-SCRAPER_IP = '0.0.0.0' #('0.0.0.0' or '127.0.0.1' for local testing, 'xxx.x.x.x' format for full online usage)
+SCRAPER_IP = '127.0.0.1' #('0.0.0.0' or '127.0.0.1' for local testing, 'xxx.x.x.x' format for full online usage)
 SCRAPER_PORT = 3000
 
 KEY_WEATHER = '81827b45a852dd349a8247994f2d47f5'
 
-# Should there be any default users or passes for this instance, before new users
-# are added via cURL command through Services API?
 scrape_user = 'user1'
 scrape_pass = 'pass1'
+
 Users = {
-        'admin':'secret',
+    'admin':'secret', # This is required for our current solution because we're directly accessing this flask
+    scrape_user:scrape_pass
 }
 
-
-# auth = HTTPBasicAuth()
 
 ### Authentication Routes ###
 @auth.verify_password
 def verify_password(username, password):
-    # user = [user for user in Users if user['id'] == task_id]
-    # if len(task) == 0:
-    #    abort(404)
-    # user = User.query.get(id)
-    # if not user:
-    #    abort(400)
-    # return jsonify({'username': user.username})
-    
-    if username == scrape_user:
-        if password == scrape_pass:
-            print(username)
-            print(password)
+    if str(username) in Users:
+        if Users[str(username)] == str(password):
             return True
-        return False
     return False
-
-
 # end of function
 
 @auth.error_handler
 def unauthorized():
-    # return 403 instead of 401 to prevent browsers from displaying the default
-    # auth dialog
+    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
     return make_response(jsonify({'error': 'Unauthorized access'}), 403)
 
 
@@ -101,7 +90,7 @@ def not_found(error):
 # ============================================================#
 @app.route('/Update', methods=['POST'])
 @auth.login_required
-def newuser():
+def addUser():
     par = reqparse.RequestParser()
     par.add_argument('user')
     par.add_argument('pass')
@@ -110,19 +99,19 @@ def newuser():
     args = par.parse_args()
     if args['new_user'] in Users:
         print('Failed: Already taken')
+        return make_response(jsonify({'Result': 'Request Failed: Already taken'}))
     else:
         newperson = {args['new_user']: args['new_pass']}
         print(newperson)
         Users.update(newperson)
         print('New person added')
-    return Users
-
+    return make_response(jsonify({'Result': 'Success!'}))
 
 # ============================================================#
 
+### Gathering Weather Data ###
 @app.route('/Weather/<string:city_name>', methods=['GET'])
 @auth.login_required
-### Gathering Weather Data ###
 def weather(city_name):
     # input city name here for testing
     #city_name = 'Blacksburg'
@@ -144,10 +133,10 @@ def weather(city_name):
     print(r.status_code)
     print(r.headers['content-type'])
     
-    # soup = BeautifulSoup(page.content, 'html.parser')
-    # page = soup.find(id = 'ResulsContainer')
-    return jsonify({'Weather Report\nlocation': city_name}, {'temperature': str(mainText['temp'])},
-                   {'pressure': str(mainText['pressure'])}, {'humidity': str(mainText['humidity'])})
+    return jsonify({'Weather Report\nlocation': city_name},
+                   {'temperature': str(mainText['temp'])},
+                   {'pressure': str(mainText['pressure'])},
+                   {'humidity': str(mainText['humidity'])})
 # end of function
 
 
@@ -156,13 +145,14 @@ def weather(city_name):
 @auth.login_required
 def covid(state_name):
     # input state name here for testing 
-   # print(state_name)
+    # print(state_name)
+    
     URL2 = 'https://www.worldometers.info/coronavirus/country/us/'
     pages = requests.get(URL2)
     soup = BeautifulSoup(pages.content, 'html.parser')
     results = soup.find(id='usa_table_countries_today')
-  #  print(results.prettify())
-   # print('checkpoint1')
+    
+    # print('checkpoint1')
     state_R = results.find('a',string=state_name)
     totalCases_R = state_R.find_next('td')
     skipField_R = totalCases_R.find_next('td')
@@ -170,7 +160,10 @@ def covid(state_name):
     skipField_R = totalDeaths_R.find_next('td')
     totalRec_R = skipField_R.find_next('td')
     
-    return jsonify({'State': str(state_R.text).strip()}, {'Total Cases': str(totalCases_R.text).strip()}, {'Total Dead': str(totalDeaths_R.text).strip()}, {'Total Recovered': str(totalRec_R.text).strip()})
+    return jsonify({'State': str(state_R.text).strip()},
+                   {'Total Cases': str(totalCases_R.text).strip()},
+                   {'Total Dead': str(totalDeaths_R.text).strip()},
+                   {'Total Recovered': str(totalRec_R.text).strip()})
 # end of function
 
 # Echo service testing route #
--- services.py
+++ services.py
@@ -3,15 +3,15 @@
 ECE 4564 - Assignment 3 - Our API (Services Flask)
 
 Before running, first make sure that the IP address you want to use is set for the parameters:
-    SERVICE_IP = '0.0.0.0'
-    SCRAPER_IP = '0.0.0.0'
+    SERVICE_IP = '127.0.0.1'
+    SCRAPER_IP = '127.0.0.1'
 (default values shown, be sure these assignments are consistent in both this file and scraper.py)
 
 Run the Service API code with the simple command line format:
     $ python3 services.py
 
 Once the flask instance is running, test connectivity from a separate terminal window with the command:
-    $ curl -u admin:secret http://0.0.0.0:8081/Test/get_services_resource
+    $ curl -u admin:secret http://127.0.0.1:8081/Test/get_services_resource
 
 Supports 4 cURL command formats:
     curl -u service_username:service_password http://127.0.0.1:8081/Marvel?story=36864
@@ -40,10 +40,10 @@ app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 # Setting default values
-SERVICE_IP = '0.0.0.0' #('0.0.0.0' or '127.0.0.1' for local testing, 'xxx.x.x.x' format for full online usage)
+SERVICE_IP = '127.0.0.1' #('0.0.0.0' or '127.0.0.1' for local testing, 'xxx.x.x.x' format for full online usage)
 SERVICE_PORT = 8081
 
-SCRAPER_IP = '0.0.0.0' #('0.0.0.0' or '127.0.0.1' for local testing, 'xxx.x.x.x' format for full online usage)
+SCRAPER_IP = '127.0.0.1' #('0.0.0.0' or '127.0.0.1' for local testing, 'xxx.x.x.x' format for full online usage)
 SCRAPER_PORT = 3000
 
 apikey = '5fd57f8f0bc35903bab675fbfc99d9f7'
@@ -51,7 +51,6 @@ privatekey = 'b790475b329908ce985571a69040077fb8f54f8d'
 
 service_username = 'admin'
 service_password = 'secret'
-#auth = HTTPBasicAuth()
 
 ### Authentication Routes ###
 @auth.verify_password
@@ -67,8 +66,7 @@ def verify_password(username, password):
 
 @auth.error_handler
 def unauthorized():
-    # return 403 instead of 401 to prevent browsers from displaying the default
-    # auth dialog
+    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
     return make_response(jsonify({'error': 'Unauthorized access'}), 403)
 
 @app.errorhandler(400)
@@ -81,9 +79,7 @@ def not_found(error):
 @app.errorhandler(405)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 405)
-#============================================================#
-# TODO: Finish adapting this section for our purposes
-#============================================================#
+
 
 ### Service Module Utility Code ###
 ###
@@ -105,30 +101,45 @@ def marvel():
     r = requests.get(url)
     finalresults = r.text
     return jsonify({'story': 'Here is the Marvel story description requested: %s' % finalresults})
-####
-# capture the sent username and password 
-# and compare to whatever the login credentials saved
-# into the python script
-####
 
-
-
-def do_admin_login():#pythonspot.com
-    if request.form['password'] == 'secret' and request.form['username'] == 'admin':
-        session['logged_in'] = True
-    else:
-        flash('Wrong password!')
-    return None
-# end of function
-
-def notFound(error):
-    return make_response(jsonify({'error':'Not found'}), 404)
-# end of function
-
+"""
+# Still debugging this method
+#@app.route('/Update', methods = ['POST'])
+#@auth.login_required
+def updateUsers():    
+    args = str(request.get_data()).split('?')
+    args_1 = args[0]
+    args_2to4 = args_2to4.split('&')
+    print(jsonify({'arg 0':arg[0]}, {'arg 1':arg[1]}), {'arg 2':arg[2]}, {'arg 3':arg[3]})
+    
+    #info = str(request.get_data()).split('&')
+    #print(info)
+    scrape_user = args_1.split('=')
+    scrape_pass = args_2to4[0].split('=')
+    new_user = args_2to4[1].split('=')
+    new_pass = args_2to4[2].split('=')
+    
+    p = scrape_pass[2].split('\'')
+    URL = "http://" + str(SCRAPER_IP) +":" +str(SCRAPER_PORT) + "/Update"
+    req = requests.get(URL, auth = (user[1], p[0]))
+    result = req.text
+    
+    #p = scrape_pass[1].split('\'')
+    
+    #URL = "http://127.0.0.1:5001/Weather/"+city_name
+    URL = "http://" + str(SCRAPER_IP) +":" +str( SCRAPER_PORT) + "/Weather/"+city_name
+    print(scrape_user[1])
+    print(scrape_pass[0])
+    print(new_user[1])
+    print(new_pass[0])
+    print(URL)
+    req = requests.get(URL, auth = (user[1], p[0]))
+    result = req.text
+    
+    return result
+"""
 
 ### Request Routes ###
-
-#one route
 @app.route('/Weather/<string:city_name>', methods = ['POST'])
 @auth.login_required
 def weatherfunct(city_name):
@@ -137,9 +148,9 @@ def weatherfunct(city_name):
     user = info[0].split('=')
     userpassword = info[1].split('=')
     p = userpassword[1].split('\'')
-    #URL = "http://127.0.0.1:5001/Weather/"+city_name
     URL = "http://" + str(SCRAPER_IP) +":" +str( SCRAPER_PORT) + "/Weather/"+city_name
     print(user[1])
+    print(p[0])
     print(userpassword[0])
     print(URL)
     req = requests.get(URL, auth = (user[1], p[0]))
@@ -147,7 +158,6 @@ def weatherfunct(city_name):
     return result
 # end of function
 
-#the second route
 @app.route('/COVID/<string:state_name>', methods = ['POST'])
 @auth.login_required
 def covid_func(state_name):
@@ -156,9 +166,10 @@ def covid_func(state_name):
     user = info[0].split('=')
     userpassword = info[1].split('=')
     p = userpassword[1].split('\'')
+    URL = "http://" + str(SCRAPER_IP) +":" + str(SCRAPER_PORT) + "/COVID/"+ state_name
     print(user[1])
     print(p[0])
-    URL = "http://" + str(SCRAPER_IP) +":" + str(SCRAPER_PORT) + "/COVID/"+ state_name
+    print(userpassword[0])
     print(URL)
     req = requests.get(URL, auth = (user[1], p[0]))
     print('Check')
